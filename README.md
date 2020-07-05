7518b16820f832b73ee5aa3a2b0976dd02fb26b7
void-v4 <mrvoidv4@gmail.com>
2020-07-05 20:06:18 +0200

    idk man just figure it out i cant be bothered to figure out what i did here
    im not very good at describing my actions :/

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Empty idle
-Empty idle is my very first attempt at making an incremental game. If you suggest a feature, it might be included in the next version!
-[Game link](https://void-v4.github.io "Empty idle")
+# empty.clickerfsdfgdgvb
+empty.clickerfsdfgdgvb is my very first attempt at making an incremental game. If you suggest a feature, it might be included in the next version!
+[Game link](https://void-v4.github.io/game "empty.clickerfsdfgdgvb")
---
+++ game/game.js
@@ -0,0 +1,47 @@
+let emptiness = 0; // this is how much empty you got
+let deleteCount = 1; // this is how much empty gets added when you click the delete button
+let deleteUpgradeCost = 10; // this is how much it costs to get upgrade the amount that the delete button deletes
+let autodeleteInterval = 1000; // this is how long the interval between autodeletes is
+let isAutodeleteBought = false; // this tells the game whether or not the autodeleter has been bought
+
+function updateCounter() {
+  document.getElementById("counter").innerHTML = "you got " + emptiness + " empty";
+  console.log("updated counter to " + emptiness "!");
+}
+
+function updateDeleteCost() {
+  document.getElementById("upgradeDeleteCount").innerHTML = "upgrade delete (cost: " + deleteUpgradeCost + " empty)";
+}
+function bruhThisThingEmpty() {
+  emptiness += deleteCount;
+  console.log("set emptiness to " + emptiness + "!");
+  updateCounter();
+}
+
+function upgradeDeleteCount() {
+  if(emptiness >= deleteUpgradeCost) {
+
+    deleteCount = deleteCount*3;
+
+    emptiness -= deleteUpgradeCost;
+    emptiness = Math.round(emptiness);
+
+    deleteUpgradeCost = deleteUpgradeCost*2.8;
+    deleteUpgradeCost = Math.round(deleteUpgradeCost);
+
+    updateCounter();
+    updateDeleteCost();
+    updateDeleteCount();
+  }
+}
+
+function updateDeleteCount() {
+  document.getElementById("deleteCount").innerHTML = "delete (" + deleteCount + ")";
+}
+
+// function autodelete() {
+//   isAutodeleteBought = true;
+//   while(isAutoDeleteBought = true) {
+//
+//   }
+// }
